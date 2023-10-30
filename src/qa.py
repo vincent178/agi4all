1536a8515ee8ad9ab656de6c52b66255fe801273
vincent178 <vh7157@gmail.com>
2023-10-30 09:19:58 +0800

    feat: RAG with history

--- src/qa.py
+++ src/qa.py
@@ -5,6 +5,7 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import RetrievalQA
 from langchain.llms import Ollama
+from langchain.chains import ConversationalRetrievalChain
 
 embedding_model_name = "thenlper/gte-base"
 
@@ -20,13 +21,27 @@ llm = Ollama(model="mistral:latest",
              temperature=0,
              callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
 
-QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt-llama")
+# QA_CHAIN_PROMPT = hub.pull("rlm/rag-prompt-llama")
 
-qa_chain = RetrievalQA.from_chain_type(
-    llm,
-    retriever=db.as_retriever(),
-    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-)
+# qa_chain = RetrievalQA.from_chain_type(
+#     llm,
+#     retriever=db.as_retriever(),
+#     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+# )
 
-question = "how to configure Docker for sneakers application?"
-result = qa_chain({"query": question})
+# question = "how to configure Docker for sneakers application?"
+# result = qa_chain({"query": question})
+
+
+qa_with_chat_history = ConversationalRetrievalChain.from_llm(llm, retriever=db.as_retriever())
+
+chat_history = []
+query = "how to configure Docker for sneakers application?"
+result = qa_with_chat_history({"question": query, "chat_history": chat_history})
+
+chat_history = [(query, result["answer"])]
+query = "use docker-compose.yml file"
+result = qa_with_chat_history({"question": query, "chat_history": chat_history})
+
+result["answer"]
+
